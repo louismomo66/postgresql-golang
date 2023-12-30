b17abdd712c4160296e2314d6b26691989b7efd6
louismomo66 <kwezi.louishg@example.com>
2023-12-30 12:31:46 +0300

    connecting to db using gorm

--- database/postgresql.go
+++ database/postgresql.go
@@ -2,24 +2,41 @@ package database
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"os"
 
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func ConnectDB() *pgxpool.Pool {
+func ConnectDB() *gorm.DB {
 	err := godotenv.Load()
 	if err != nil {
 		log.Printf("Error loading .env file: %v\n", err)
 		os.Exit(1)
 	}
-	databasUrl := os.Getenv("DATABASE_URL")
-	dbpool, err := pgxpool.New(context.Background(), databasUrl)
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	// using pgx as the undeerlying driver for the standard database/sql package
+	sqlDB, err := sql.Open("pgx", databaseURL)
 	if err != nil {
-		log.Fatalf("Failed to connect to db")
+		log.Fatalf("Failed to open a DB connection: %v", err)
 	}
-	log.Printf("Connected to database")
-	return dbpool
+	// make sure the database is reachable
+	err = sqlDB.PingContext(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to ping DB: %v", err)
+
+	}
+	// Initialize GORM with the sql.DB object
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to initialize GORM: %v", err)
+	}
+	log.Println("Connected to database using GORM and pgx")
+	return gormDB
 }
--- models/person.go
+++ models/person.go
@@ -5,8 +5,11 @@ import (
 )
 
 type Person struct {
-	ID          int
-	FirstName   string
-	LastName    string
-	DateOfBirth time.Time
+	ID          int       `gorm:"primarykey"`
+	FirstName   string    `gorm:"type:varchar(100)"`
+	LastName    string    `gorm:"type:varchar(100)"`
+	DateOfBirth time.Time `gorm:"type:date"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   time.Time `gorm:"index"`
 }
